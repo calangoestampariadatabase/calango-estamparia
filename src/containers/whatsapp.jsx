55f9ceb33c2787228f6793256f0b151e87d7e784
calangoestampariadatabase <calangoestampariadatabase@gmail.com>
2026-05-08 10:15:43 -0300

    Tintim no Pop-up WhatsApp

--- src/containers/whatsapp.jsx
+++ src/containers/whatsapp.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const Whatsapp = () => {
   return (
     <div className="p-4 text-[40px] bg-[#25D366] text-[#FDFBFE] rounded-full  hover:bg-[#01D366] transition-colors duration-300 fixed bottom-6 right-6 shadow-lg cursor-pointer">
-      <a href="https://wa.me/5577988740276?text=Ol%C3%A1%2C%20vim%20do%20catalogo%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento" target="_blank" rel="noopener noreferrer" aria-label="Converse pelo WhatsApp">
+      <a href="https://tintim.link/whatsapp/11059be2-0410-4b61-8aae-918b6f3b5322/0fb84df1-b732-4d91-acdb-8caae04c2fbb" target="_blank" rel="noopener noreferrer" aria-label="Converse pelo WhatsApp">
       <FaWhatsapp />
       </a>
     </div>
